de8a6023160520f41159ec604b67e2dff06870f0
Warren Harper II <warrenharper2@att.net>
2019-08-28 15:52:41 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,50 @@
 class BankAccount
+  checkdeposit = 0
+checkwithdrawal = 0
+checkbalancetotal = 0
 
+savingsdeposit = 0
+savingswithdrawal = 0
+savingsbalancetotal = 0
+
+def CheckSaving():
+  z=int(input("Which account would you like to use?")
+   ("If you choose Checkings, press 1")
+     if CheckSaving == 1
+    a=int(input("Would you like to deposit or withdrawal in your checkings account?"))
+    if a == "deposit":
+      A=int(input("How much would you like to deposit?"))
+    elif a == "withdrawal":
+      A1=int(input("How much would you like to withdrawal?"))
+
+     ("If you choose Savings, press 2")
+       elif CheckSaving == 2:
+    b=int(input("Would you like to deposit or withdrawal in your savings account?"))
+    if b == "deposit":
+      B=int(input("How much would you like to deposit?"))
+    elif b == "withdrawal":
+      B1=int(input("How much would you like to withdrawal"))
+
+     ("If you choose Deposit, press 3")
+       elif CheckSaving == 3:
+    c=input("Would you like to deposit?")
+    if c == "yes":
+      C=int(input("How much would you like to deposit?"))
+    elif c == +1:
+      print("Ok")
+    elif c == "no":
+      print("Ok")
+
+     ("If you choose Withdrawal, press 4")
+       elif CheckSaving == 4:
+    d=input("Would you like to withdrawal?")
+    if d == "yes":
+      D=int(input("How much would you like to withdrawal?"))
+    elif d == "no":
+      print("Ok")
+
+  else:
+    print("Please start again")
+
+CheckSaving()
 end
